6e7270b586fbd90e34955f85a08d5dc391e635ec
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-02-21 12:29:51 -0600

    feat(web): add tipografia poppins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@fontsource/poppins/index.css";`
+        }
+      }
+    }
+  }
+});
+
